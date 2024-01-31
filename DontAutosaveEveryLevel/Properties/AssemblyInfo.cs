92aa89c26cb387f953c6302396d755ed3013e9b4
v1ld <v1ld.git@gmail.com>
2024-01-31 16:41:17 -0500

    Added DontAutosaveEveryLevel mod.

---
+++ DontAutosaveEveryLevel/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DontAutosaveEveryLevel")]
+[assembly: AssemblyDescription("Don't Autosave Every Level")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DontAutosaveEveryLevel")]
+[assembly: AssemblyCopyright("Copyright ©v1ld, Jan 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("177AA022-2D3B-4000-A3DE-901D3EE4D5AA")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DontAutosaveEveryLevel/mod_DontAutosaveEveryLevel.cs
@@ -0,0 +1,32 @@
+﻿using Game;
+using Patchwork;
+using System;
+
+namespace V1ld_DontAutosaveEveryLevel
+{
+    [ModifiesType]
+    public class V1ld_GameState_DAEL : GameState
+    {
+        [NewMember]
+        public static DateTime V1ld_LastAutoSaveTime;
+
+        [ModifiesMember("Autosave")]
+        new public static void Autosave()
+        {
+            if (Option.TrialOfIron)
+            {
+                TrialOfIronSave(viaApplicationQuit: false);
+                return;
+            }
+
+            var now = DateTime.Now;
+            var minutes = 10f;
+            if ((minutes >= 0) && ((now - V1ld_LastAutoSaveTime).TotalMinutes >= minutes))
+            {
+                SaveLoadManager.SaveGame(SaveLoadManager.SaveGameType.Autosave, string.Empty);
+                Instance.AutosaveCycleNumber++;
+                V1ld_LastAutoSaveTime = now;
+            }
+        }
+    }
+}
